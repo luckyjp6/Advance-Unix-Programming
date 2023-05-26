d4beab9465c1573cfa5e6f2ddf81a0434878ae7f
luckyjp6 <luckyjp6.cs09@nycu.edu.tw>
2023-05-26 09:02:51 +0800

    recover/set breakpoint

--- hw2/sdb.c
+++ hw2/sdb.c
@@ -16,11 +16,134 @@ int child_stat;
 struct user_regs_struct regs, anchor_regs;
 char cmd[100];
 
-int bps_idx = 0;
+int bps_idx = 0, wait_to_set = -1;
 struct break_point{
     unsigned long long int addr;
     uint8_t value;
 }bps[1000];
+void clear_bp(int idx);
+int get_empty_bp();
+
+void err_quit(char *msg);
+
+void read_args();
+
+void set_break(unsigned long long int b_addr, bool is_new);
+void recover_break(int idx, bool msg);
+
+int check_bp(unsigned long long int now);
+int wait_stop();
+void print_insturctions();
+int do_next();
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("usage: ./sdb [program]\n");
+        return 0;
+    }
+
+    unsigned long long int entry_point; 
+    unsigned long long int magic_addr;
+
+    if((child = fork()) < 0) err_quit("fork");
+    if (child == 0) {
+        if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) err_quit("ptrace traceme");
+        execvp(argv[1], argv+1);
+        err_quit("execvp");
+    }
+
+    /* clear bps */
+    for (int i = 0; i < 1000; i++) clear_bp(i);
+
+    /* open capstone handler */
+    if (cs_open(CS_ARCH_X86, CS_MODE_64, &cs_handle) != CS_ERR_OK) {
+        perror("capstone");
+        return -1;
+    }
+    // if (ptrace(PTRACE_ATTACH, child, 0, 0) < 0) err_quit("ptrace attach");
+    
+    if (waitpid(child, &child_stat, 0) < 0) err_quit("waitpid");
+    if (ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL) < 0) err_quit("ptrace set options");
+    if (!WIFSTOPPED(child_stat)) err_quit("child not stop");
+
+    // int fd;
+    // char path[100] = {0}, buf[200000];
+    // sprintf(path, "/proc/%d/maps", child);
+    // fd = open(path, O_RDONLY);
+    // if (fd < 0) err_quit("open child maps");
+    // printf("open at %d\n", fd); fflush(stdout);
+    // int r_len = read(fd, buf, sizeof(buf));
+    // if (r_len < 0) err_quit("read child maps");
+    // if (r_len == 0) err_quit("EOF??");
+    // write(1, buf, r_len);
+
+    /* start child */
+    read_args();
+    entry_point = regs.rip;
+    printf("** program '%s' loaded. entry point 0x%llx\n", argv[1], entry_point);
+    print_insturctions();
+    while (true) {
+        int cmd_len, now_bp;
+        memset(cmd, 0, 100);
+
+        printf("(sdb): "); fflush(stdout);
+        cmd_len = read(0, cmd, 100);
+        if (cmd_len < 0) err_quit("stdin");
+        if (cmd_len == 0) break;
+
+        if (memcmp(cmd, "exit", 4) == 0) break;
+        else if (memcmp(cmd, "si", 2) == 0) {
+            if (ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) err_quit("single step");
+            if (do_next() > 0) break; // client exit
+        }
+        else if (memcmp(cmd, "cont", 4) == 0) {
+            /* single step and recover break point */
+            if (ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) err_quit("single step");
+            if (wait_stop() > 0) break;
+            if (wait_to_set >= 0) {
+                set_break(bps[wait_to_set].addr, false);
+                wait_to_set = -1;
+            }
+
+            read_args();
+            int idx = check_bp(regs.rip);
+            if (idx >= 0) recover_break(idx, true);
+            else { if (ptrace(PTRACE_CONT, child, 0, 0) < 0) err_quit("cont"); }
+            if (do_next() > 0) break; // client exit
+        }else if (memcmp(cmd, "break", 5) == 0) {
+            unsigned long long int b_addr;
+            char *location = cmd;
+            char *tmp_cmd = strtok_r(location, " ", &location);
+            sscanf(location, "%llx", &b_addr);
+            if (check_bp(b_addr) < 0) set_break(b_addr, true);
+            else printf("** set a breakpoint at 0x%llx.\n", b_addr);
+        }else if (memcmp(cmd, "anchor", 6) == 0) {
+            if (ptrace(PTRACE_GETREGS, child, 0, &anchor_regs) < 0) err_quit("ptrace get regs");
+            printf("** dropped an anchor\n");
+        }else if (memcmp(cmd, "timetravel", 10) == 0) {
+            if (ptrace(PTRACE_SETREGS, child, 0, &anchor_regs) < 0) err_quit("ptrace set regs");      
+            printf("** go back to the anchor point\n");
+            read_args();
+
+            if (wait_to_set >= 0) {
+                set_break(bps[wait_to_set].addr, false);
+                wait_to_set = -1;
+            }
+            
+            int idx = check_bp(regs.rip);
+            if (idx >= 0) recover_break(idx, false);
+
+            print_insturctions();
+        }
+
+        fflush(stdout);
+    }
+
+    cs_close(&cs_handle);
+    return 0;
+}
+
+
 void clear_bp(int idx) {
     bps[idx].addr = -1;
     bps[idx].value = -1;
@@ -28,6 +151,8 @@ void clear_bp(int idx) {
     if (idx == bps_idx && idx > 0) bps_idx -= 1;
 }
 int get_empty_bp() {
+    return bps_idx++;
+
     int i;
     for (i = 0; i < bps_idx; i++) {
         if ((bps[i].addr == -1) && (bps[i].value == -1)) break;
@@ -46,7 +171,7 @@ void read_args() {
     if (ptrace(PTRACE_GETREGS, child, 0, &regs) < 0) err_quit("ptrace get regs");
 }
 
-void set_break(unsigned long long int b_addr) {
+void set_break(unsigned long long int b_addr, bool is_new) {
     /* get original instruction */
     unsigned long int tmp_codes;
     tmp_codes = ptrace(PTRACE_PEEKTEXT, child, b_addr, 0);
@@ -56,17 +181,19 @@ void set_break(unsigned long long int b_addr) {
     }
 
     /* recored */
-    int bp = get_empty_bp();
-    bps[bp].addr = b_addr;
-    bps[bp].value = tmp_codes & 0xff;
+    if (is_new) {
+        int bp = get_empty_bp();
+        bps[bp].addr = b_addr;
+        bps[bp].value = tmp_codes & 0xff;
+    }
 
     /* set 0xcc */
     if (ptrace(PTRACE_POKETEXT, child, b_addr, (tmp_codes & 0xffffffffffffff00) | 0xcc) < 0) err_quit("ptrace poketext");
    
-    printf("** set a breakpoint at 0x%llx.\n", b_addr);
+    if (is_new) printf("** set a breakpoint at 0x%llx.\n", b_addr);
     return;
 }
-void recover_break(int idx) {
+void recover_break(int idx, bool msg) {
     /* get current instruction */
     unsigned long int tmp_codes;
     tmp_codes = ptrace(PTRACE_PEEKTEXT, child, bps[idx].addr, 0);
@@ -78,14 +205,14 @@ void recover_break(int idx) {
     /* recover original instruction */
     if (ptrace(PTRACE_POKETEXT, child, bps[idx].addr, (tmp_codes & 0xffffffffffffff00) | bps[idx].value) < 0) err_quit("ptrace poketext");
     if (memcmp(cmd, "cont", 4) == 0) {
-        printf("is cont\n");
         regs.rip --;
         if (ptrace(PTRACE_SETREGS, child, 0, &regs) < 0) err_quit("ptrace set regs");
     }
 
-    printf("** hit a breakpoint at 0x%llx\n", bps[idx].addr);
+    if (msg) printf("** hit a breakpoint at 0x%llx\n", bps[idx].addr);
 
-    clear_bp(idx);
+    wait_to_set = idx;
+    // clear_bp(idx);
     return;
 }
 
@@ -151,6 +278,10 @@ void print_insturctions() {
 int do_next() {
     /* wait client end */
     if (wait_stop() > 0) return 1;
+    if (wait_to_set >= 0) {
+        set_break(bps[wait_to_set].addr, false);
+        wait_to_set = -1;
+    }
     
     /* get new args */
     read_args();
@@ -158,93 +289,8 @@ int do_next() {
     int idx;
     if (memcmp(cmd, "cont", 4) == 0) idx = check_bp(regs.rip -1);
     if (memcmp(cmd, "si", 2) == 0) idx = check_bp(regs.rip);
-    if (idx >= 0) recover_break(idx);
+    if (idx >= 0) recover_break(idx, true);
 
     print_insturctions();
-    return 0;
-}
-
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("usage: ./sdb [program]\n");
-        return 0;
-    }
-
-    unsigned long long int entry_point; 
-    unsigned long long int magic_addr;
-
-    if((child = fork()) < 0) err_quit("fork");
-    if (child == 0) {
-        if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) err_quit("ptrace traceme");
-        execvp(argv[1], argv+1);
-        err_quit("execvp");
-    }
-
-    /* clear bps */
-    for (int i = 0; i < 1000; i++) clear_bp(i);
-
-    /* open capstone handler */
-    if (cs_open(CS_ARCH_X86, CS_MODE_64, &cs_handle) != CS_ERR_OK) {
-        perror("capstone");
-        return -1;
-    }
-    // if (ptrace(PTRACE_ATTACH, child, 0, 0) < 0) err_quit("ptrace attach");
-    
-    if (waitpid(child, &child_stat, 0) < 0) err_quit("waitpid");
-    if (ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL) < 0) err_quit("ptrace set options");
-    if (!WIFSTOPPED(child_stat)) err_quit("child not stop");
-
-    // int fd;
-    // char path[100] = {0}, buf[200000];
-    // sprintf(path, "/proc/%d/maps", child);
-    // fd = open(path, O_RDONLY);
-    // if (fd < 0) err_quit("open child maps");
-    // printf("open at %d\n", fd); fflush(stdout);
-    // int r_len = read(fd, buf, sizeof(buf));
-    // if (r_len < 0) err_quit("read child maps");
-    // if (r_len == 0) err_quit("EOF??");
-    // write(1, buf, r_len);
-
-    /* start child */
-    read_args();
-    entry_point = regs.rip;
-    printf("** program '%s' loaded. entry point 0x%llx\n", argv[1], entry_point);
-    print_insturctions();
-    while (true) {
-        int cmd_len, now_bp;
-        memset(cmd, 0, 100);
-
-        printf("(sdb): "); fflush(stdout);
-        cmd_len = read(0, cmd, 100);
-        if (cmd_len < 0) err_quit("stdin");
-        if (cmd_len == 0) break;
-
-        if (memcmp(cmd, "exit", 4) == 0) break;
-        else if (memcmp(cmd, "si", 2) == 0) {
-            if (ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) err_quit("single step");
-            if (do_next() > 0) break; // client exit
-        }
-        else if (memcmp(cmd, "cont", 4) == 0) {
-            if (ptrace(PTRACE_CONT, child, 0, 0) < 0) err_quit("cont");
-            if (do_next() > 0) break; // client exit
-        }else if (memcmp(cmd, "break", 5) == 0) {
-            unsigned long long int b_addr;
-            char *location = cmd;
-            char *tmp_cmd = strtok_r(location, " ", &location);
-            sscanf(location, "%llx", &b_addr);
-            set_break(b_addr);
-        }else if (memcmp(cmd, "anchor", 6) == 0) {
-            if (ptrace(PTRACE_GETREGS, child, 0, &anchor_regs) < 0) err_quit("ptrace get regs");
-            printf("** dropped an anchor\n");
-        }else if (memcmp(cmd, "timetravel", 10) == 0) {
-            if (ptrace(PTRACE_SETREGS, child, 0, &anchor_regs) < 0) err_quit("ptrace set regs");      
-            printf("** go back to the anchor point\n");
-            print_insturctions();
-        }
-
-        fflush(stdout);
-    }
-
-    cs_close(&cs_handle);
     return 0;
 }
